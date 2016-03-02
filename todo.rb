6b023b66e6339c5d705c21fb8d39246b2cd3d42d
William Reilly <William.Reilly2010@gmail.com>
2016-03-01 19:56:25 -0500

    Can add a new list

--- todo.rb
+++ todo.rb
@@ -1,7 +1,32 @@
 require "sinatra"
-require "sinatra/reloader"
+require "sinatra/reloader" if development?
 require "tilt/erubis"
 
-get "/" do
-  erb "You have no lists.", layout: :layout
+require 'pry'
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret' # give 'secret' so that Sinatra's behavior isn't setting this everytime server is restarted
+end
+
+before do
+  session[:lists] ||= []
+  @lists = session[:lists]
+end
+
+get "/lists" do
+  erb :lists, layout: :layout
+end
+
+get '/lists/new' do
+  erb :new_list, layout: :layout  
+end
+
+post '/lists' do
+  session[:lists] << {name: params[:list_name], todos: []}
+  redirect '/lists'
+end
+
+not_found do
+  redirect '/lists'
 end
