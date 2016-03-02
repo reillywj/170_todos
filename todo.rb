24ac1107ef9648f27cbfcc09679c2d6bab382f74
William Reilly <William.Reilly2010@gmail.com>
2016-03-01 20:02:55 -0500

    Notes on URLs

--- todo.rb
+++ todo.rb
@@ -14,19 +14,32 @@ before do
   @lists = session[:lists]
 end
 
+# GET /lists        -> view all lists
+# GET /lists/new    -> new list form
+# POST /lists       -> create new list
+# GET /lists/1      -> view a single list
+# GET /lists/1/new  -> new todo form
+# POST /lists/1     -> create a new todo
+
 get "/lists" do
   erb :lists, layout: :layout
 end
 
+# Render for for new list
 get '/lists/new' do
   erb :new_list, layout: :layout  
 end
 
+# Posting a new list; redirect to '/lists'
 post '/lists' do
   session[:lists] << {name: params[:list_name], todos: []}
   redirect '/lists'
 end
 
-not_found do
+# not_found do
+#   redirect '/lists'
+# end
+
+get '/' do
   redirect '/lists'
 end
