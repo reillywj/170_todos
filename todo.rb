7393e65d26b733e252eeb77567cac647d4764174
William Reilly <William.Reilly2010@gmail.com>
2016-03-02 22:07:13 -0500

    Refactored for Rubocop

--- todo.rb
+++ todo.rb
@@ -1,12 +1,14 @@
-require "sinatra"
-require "sinatra/reloader" if development?
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'tilt/erubis'
 
 require 'pry'
 
 configure do
   enable :sessions
-  set :session_secret, 'secret' # give 'secret' so that Sinatra's behavior isn't setting this everytime server is restarted
+  # give 'secret'
+  # Otherwise Sinatra resets this value every time
+  set :session_secret, 'secret'
 end
 
 before do
@@ -21,20 +23,21 @@ end
 # GET /lists/1/new  -> new todo form
 # POST /lists/1     -> create a new todo
 
-get "/lists" do
+# Main page showing all lists
+get '/lists' do
   erb :lists, layout: :layout
 end
 
 # Render for for new list
 get '/lists/new' do
-  erb :new_list, layout: :layout  
+  erb :new_list, layout: :layout
 end
 
 # Returns an error message if the name is invalid.
 def list_validation_error(name)
   if !(1..100).cover? name.size
     'List name must be between 1 and 100.'
-  elsif session[:lists].any? { |list| list[:name] == name}
+  elsif session[:lists].any? { |list| list[:name] == name }
     "'#{name}' already exists."
   end
 end
@@ -42,14 +45,14 @@ end
 # Posting a new list; redirect to '/lists'
 post '/lists' do
   list_name = params[:list_name].strip
-  
+
   error = list_validation_error(list_name)
-  
+
   if error
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    session[:lists] << {name: list_name, todos: []}
+    session[:lists] << { name: list_name, todos: [] }
     session[:success] = 'The list has been created.'
     redirect '/lists'
   end
