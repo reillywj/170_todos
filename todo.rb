40828240f50e761ee540ae8372c3929bae1fcdfb
William Reilly <William.Reilly2010@gmail.com>
2016-04-17 20:31:50 -0400

    Commented out pry gem.

--- todo.rb
+++ todo.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/content_for'
 require 'sinatra/reloader' if development?
 require 'tilt/erubis'
-require 'pry' if development?
+# require 'pry' if development?
 
 configure do
   enable :sessions
