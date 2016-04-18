ce81471f4b678ba1ace6e7a1b498ac2f36cbe6f4
William Reilly <William.Reilly2010@gmail.com>
2016-04-17 20:24:01 -0400

    Updated lists to have ids.

--- todo.rb
+++ todo.rb
@@ -33,8 +33,8 @@ helpers do
     "complete" if todo[:completed]
   end
 
-  def list_path(list_id)
-    "/list/#{list_id}"
+  def list_path(list)
+    "/list/#{list[:id]}"
   end
 
   def sort_lists(lists, &some_block)
@@ -59,27 +59,40 @@ before do
   @lists = session[:lists]
 end
 
+def redirect_and_halt
+  redirect '/lists'
+  halt
+end
+
 # Loads a list and redirects if not found
-def load_list(index)
-  list = session[:lists][index] if index
+def load_list(id)
+  list = session[:lists].find{|list| list[:id] == id} || nil
   return list if list
 
   session[:error] = "The specified list was not found."
-  redirect "/lists"
-  halt
+  redirect_and_halt
+end
+
+# Loads a todo and redirects if not found
+def load_todo(list, todo_id)
+  todo = list[:todos].find{|todo| todo[:id] == todo_id } || nil
+  return todo if todo
+
+  session[:error] = "The specified todo was not found."
+  redirect_and_halt
 end
 
 # common list setup
 def list_setup
-  @list_id = params[:list_id].to_i
-  @list = load_list @list_id
+  @list = load_list params[:list_id].to_i
 end
 
 # Returns an error message if the name is invalid.
-def list_validation_error(name)
+def list_validation_error(name, lists = nil)
+  lists = lists || session[:lists]
   if !(1..100).cover? name.size
     'List name must be between 1 and 100.'
-  elsif session[:lists].any? { |list| list[:name] == name }
+  elsif lists.any? { |list| list[:name] == name }
     "'#{name}' already exists."
   end
 end
@@ -94,15 +107,15 @@ def todo_validation_error(input_todo, list)
 end
 
 # Validate List name while editing
-def edit_list_name_validation(list_number, new_name)
-  other_lists = session[:lists].map.to_a
-  other_lists.delete_at list_number
+def edit_list_name_validation(this_list_id, new_name)
+  other_lists = session[:lists].reject{|list| list[:id] == this_list_id }
 
-  if !(1..100).cover? new_name.size
-    "List name must be between 1 and 100."
-  elsif other_lists.any? { |list| list[:name] == new_name }
-    "'#{new_name}' already exists."
-  end 
+  # if !(1..100).cover? new_name.size
+  #   "List name must be between 1 and 100."
+  # elsif other_lists.any? { |list| list[:name] == new_name }
+  #   "'#{new_name}' already exists."
+  # end 
+  list_validation_error(new_name, other_lists)
 end
 
 # GET /lists        -> view all lists
@@ -122,6 +135,11 @@ get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
+def next_id(elements)
+  max = elements.map {|list| list[:id] }.max || 0
+  max + 1
+end
+
 # Posting a new list; redirect to '/lists'
 post '/lists' do
   list_name = params[:list_name].strip
@@ -132,7 +150,9 @@ post '/lists' do
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    session[:lists] << { name: list_name, todos: [] }
+    lists = session[:lists]
+    id = next_id(lists)
+    lists << { id: id, name: list_name, todos: [] }
     session[:success] = 'The list has been created.'
     redirect '/lists'
   end
@@ -148,14 +168,6 @@ get '/list/:list_id' do
   erb :list, layout: :layout
 end
 
-def next_todo_id(todos)
-  # get list of todos
-  # find greatest id in todos
-  max = todos.map { |todo| todo[:id] }.max || 0
-  # increment
-  max + 1
-end
-
 # Posting a new todo to list; redirect to '/list/:number'
 post '/list/:list_id/todos' do
   todo = params[:todo].strip
@@ -165,10 +177,10 @@ post '/list/:list_id/todos' do
     session[:error] = error
     erb :list, layout: :layout
   else
-    id = next_todo_id(@list[:todos])
+    id = next_id(@list[:todos])
     @list[:todos] << {id: id, name: todo, completed: false}
     session[:success] = 'Todo added successfully.'
-    redirect "/list/#{@list_id}"
+    redirect list_path(@list)
   end
 end
 
@@ -183,7 +195,7 @@ post '/list/:list_id' do
   list_setup
   list_name = params[:list_name].strip
 
-  error = edit_list_name_validation(@list_id, list_name)
+  error = edit_list_name_validation(@list[:id], list_name)
   if error
     session[:error] = error
     erb :edit_list, layout: :layout
@@ -195,14 +207,13 @@ post '/list/:list_id' do
       @list[:name] = list_name
       session[:success] = 'List updated successfully.'
     end
-
-    redirect "/list/#{@list_id}"
+    redirect list_path(@list)
   end
 end
 
 # Delete a todo list
 post '/list/:list_id/delete' do
-  session[:lists].delete_at params[:list_id].to_i
+  session[:lists].reject! { |list| list[:id]  = params[:list_id].to_i }
 
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     "/lists"
@@ -215,9 +226,9 @@ end
 # Delete a todo from a list
 post '/list/:list_id/todo/:todo_id/delete' do
   list_id = params[:list_id].to_i
+  list = load_list(list_id)
   todo_id = params[:todo_id].to_i
-  # session[:lists][list_id][:todos].delete_at todo_id
-  session[:lists][list_id][:todos].reject! { |todo| todo[:id] == todo_id }
+  list[:todos].reject! { |todo| todo[:id] == todo_id }
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     # ajax
     status 204 # successful but no content
@@ -231,7 +242,8 @@ end
 post '/list/:list_id/todo/:todo_id' do
   list_id = params[:list_id].to_i
   todo_id = params[:todo_id].to_i
-  todo = session[:lists][list_id][:todos].find { |todo| todo[:id] == todo_id }
+  list = load_list(list_id)
+  todo = find_todo(list, todo_id)
   is_completed = params[:completed] == "true"
   todo[:completed] =  is_completed
   session[:success] = "#{todo[:name]} has been #{is_completed ? "checked" : "unchecked"}."
@@ -241,17 +253,21 @@ end
 # POST to complete all todos in a list
 post '/list/:list_id/complete_all' do
   list_id = params[:list_id].to_i
-  session[:lists][list_id][:todos].each do |todo|
-    todo[:completed] = true
+  list = load_list(list_id)
+  todos = list[:todos]
+  if todos.empty?
+    session[:error] = "There are no todos in #{list[:name]}."
+  else
+    list[:todos].each { |todo| todo[:completed] = true }
+    session[:success] = "All todos were marked completed."
   end
-  session[:success] = "All todos were marked completed."
   redirect "/list/#{list_id}"
 end
 
-not_found do
+get '/' do
   redirect '/lists'
 end
 
-get '/' do
+not_found do
   redirect '/lists'
 end
